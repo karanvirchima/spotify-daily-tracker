5fb23511535a4ab9943515c135c884564ae23984
karanvirchima <91230405+karanvirchima@users.noreply.github.com>
2023-12-22 14:47:47 -0800

    adding all files neccessary to run on localhost

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+# Import the required functions from spotify_tracker module
+from spotify_tracker import get_time_listened, get_top_artists, get_genres_from_top_artists
+
+app = Flask(__name__)
+
+def create_spotify_client():
+    """Creates and returns a new Spotipy client instance."""
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="d949fdef00594bcd98e6dd5ee250e0a8",
+                                                     client_secret="d9bba50de97d46838b716796b9259be1",
+                                                     redirect_uri="http://localhost:8888/callback",
+                                                     scope="user-top-read user-read-recently-played"))
+
+@app.route('/')
+def index():
+    sp = create_spotify_client()
+    total_time_listened = get_time_listened(sp)  # Pass sp to the function
+    top_artists = get_top_artists(sp)  # Pass sp to the function
+    genres_listened = get_genres_from_top_artists(top_artists)  # Pass top_artists to the function
+    return render_template('index.html', total_time=total_time_listened, top_artists=top_artists, genres_listened=genres_listened)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- spotify_tracker.py
+++ spotify_tracker.py
@@ -1,11 +1,11 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import datetime
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="d949fdef00594bcd98e6dd5ee250e0a8",
-                                               client_secret="4e3f8248eae6443c9edf9203c4a56a80",
-                                               redirect_uri="http://localhost:8888/callback",
-                                               scope="user-top-read user-read-recently-played"))
+def create_spotify_client():
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="d949fdef00594bcd98e6dd5ee250e0a8",
+                                                     client_secret="d9bba50de97d46838b716796b9259be1",
+                                                     redirect_uri="http://localhost:8888/callback",
+                                                     scope="user-top-read user-read-recently-played"))
 
 def get_time_listened(sp):
     total_time = 0
@@ -25,6 +25,7 @@ def get_genres_from_top_artists(top_artists):
     return list(genres)
 
 def main():
+    sp = create_spotify_client()
     total_time_listened = get_time_listened(sp)
     top_artists = get_top_artists(sp)
     genres_listened = get_genres_from_top_artists(top_artists)
@@ -33,6 +34,19 @@ def main():
     print("Top Artists:", top_artists)
     print("Genres Listened:", genres_listened)
 
+# The Flask app part would be in a separate file
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     sp = create_spotify_client()
+#     total_time_listened = get_time_listened(sp)
+#     top_artists = get_top_artists(sp)
+#     return render_template('index.html', total_time=total_time_listened, top_artists=top_artists)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == "__main__":
     main()
-
